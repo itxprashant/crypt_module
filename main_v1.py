73800dbbc2c0f5674be0490fd092d63403f205c6
Prashant Tiwari <prashantt492@gmail.com>
2022-01-15 01:48:42 +0530

    first commit

---
+++ main_v1.py
@@ -0,0 +1,34 @@
+alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZab1cd3e2f4ghi5jkl6mn7opq8rs9 t0uvwxyz"
+fu = open("key","r")
+mykey = fu.read()
+
+
+def create_key(key_text):
+    pass
+    
+def encrypt(text, key):
+
+    xkey = key
+    pairs_enc = {alphabets[i]: xkey[i] for i in range(len(alphabets))}
+    enc_text = str()
+    for i in range(len(text)):        
+        enc_text += pairs_enc[text[i]]
+ 
+
+    return enc_text
+
+
+
+
+def decrypt(text, key):
+
+    xkey = key
+    pairs_dec = {alphabets[i]: xkey[i] for i in range(len(alphabets))}
+    dec_text = str()
+    for i in range(len(text)):        
+        dec_text += pairs_enc[text[i]]
+ 
+
+    return dec_text
+print(encrypt("Hello There", mykey))
+fu.close()
