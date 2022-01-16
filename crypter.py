faf8ea5b304b07cffec8bc097ddd397d151f973b
Prashant Tiwari <prashantt492@gmail.com>
2022-01-16 20:26:46 +0530

    Removed indentation bugs

--- crypter.py
+++ crypter.py
@@ -6,19 +6,18 @@ e - encrypt a text
 d - decrypt a text.\n""")
 while True:
     x = input("Enter a command:").lower()
-    match x:
-        case 'g':
-            mod.create_key()
-            print("Generated key: ",mod.read_key()) 
-        case 'i':
-            f = open("key","w")
-            y = input("Enter a key(must be a generated one): ")
-            f.write(y)
-            f.close()
-            print("Key added successfully!")
-        case 'e':
-            z = input("Enter a text to encrypt: ")
-            print("Encrypted text: ", mod.encrypt(z, mod.read_key()))
-        case 'd':
-            zz = input("Enter a text to decrypt: ")
-            print("Decrypted text: ", mod.decrypt(zz, mod.read_key()))
+    if x == 'g':
+        mod.create_key()
+        print("Generated key: ",mod.read_key()) 
+    elif x == 'i':
+        f = open("key","w")
+        y = input("Enter a key(must be a generated one): ")
+        f.write(y)
+        f.close()
+        print("Key added successfully!")
+    elif x == 'e':
+        z = input("Enter a text to encrypt: ")
+        print("Encrypted text: ", mod.encrypt(z, mod.read_key()))
+    elif x == 'd':
+        zz = input("Enter a text to decrypt: ")
+        print("Decrypted text: ", mod.decrypt(zz, mod.read_key()))
